946a90301ec06f701b934dc46472b32099316547
CaptainCrunch11 <ccole2002@gmail.com>
2026-01-16 18:41:33 -0500

    Stop tracking local config.py

--- Media/config.py
+++
@@ -1,7 +0,0 @@
-# Enter absolute paths to:
-playlist_folder = '...' # the folder with the playlist text files
-video_folder = '...' # the folder that holds videos
-song_folder = '...' # the folder that holds songs (If you have a parent songFolder with a subfolder for a group of songs like a game ost, use the parent folder.)
-audio_folder = '...' # the folder that holds audio clips
-image_folder = '...' # the folder that holds images
-gif_folder = '...' # the folder that holds gifs
