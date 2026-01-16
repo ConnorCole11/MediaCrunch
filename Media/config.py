564578f879673c886c0cabfcf18cfbe719e8c628
CaptainCrunch11 <ccole2002@gmail.com>
2026-01-16 14:50:57 -0500

    Update config.py
    typo

--- Media/config.py
+++ Media/config.py
@@ -4,5 +4,5 @@ video_folder = '...' # the folder that holds videos
 song_folder = '...' # the folder that holds songs (If you have a parent songFolder with a subfolder for a group of songs like a game ost, use the parent folder.)
 audio_folder = '...' # the folder that holds audio clips
 image_folder = '...' # the folder that holds images
-gif_folder = '...' # the fodler that holds gifs
+gif_folder = '...' # the folder that holds gifs
 
