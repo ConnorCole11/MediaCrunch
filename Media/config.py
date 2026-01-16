aa2fa4f74d50ae5f920f960447bee510976e6295
CaptainCrunch11 <ccole2002@gmail.com>
2026-01-16 14:50:24 -0500

    Update config.py
    Added gif_folder variable

--- Media/config.py
+++ Media/config.py
@@ -1,8 +1,8 @@
 # Enter absolute paths to:
-playlist_folder = ... # the folder with the playlist text files
-video_folder = ... # the folder that holds videos
-song_folder = ... # the folder that holds songs (If you have a parent songFolder with a subfolder for a group of songs like a game ost, use the parent folder.)
-audio_folder = ... # the folder that holds audio clips
-image_folder = ... # the folder that holds images
-
+playlist_folder = '...' # the folder with the playlist text files
+video_folder = '...' # the folder that holds videos
+song_folder = '...' # the folder that holds songs (If you have a parent songFolder with a subfolder for a group of songs like a game ost, use the parent folder.)
+audio_folder = '...' # the folder that holds audio clips
+image_folder = '...' # the folder that holds images
+gif_folder = '...' # the fodler that holds gifs
 
