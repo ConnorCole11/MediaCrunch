64b2d799afd2fee01b3dd33ab1b4f320ca2eba94
CaptainCrunch11 <ccole2002@gmail.com>
2026-01-16 20:49:08 -0500

    Added stuff to main to test

--- Media/main.py
+++ Media/main.py
@@ -3,9 +3,6 @@ try:
 except ImportError: 
     from config import *
 
-<<<<<<< HEAD
-=======
 from music.makePlaylist.main import makePlaylist_main
 
 makePlaylist_main()
->>>>>>> e7ff8b6 (Created the config class, and top main file (2))
